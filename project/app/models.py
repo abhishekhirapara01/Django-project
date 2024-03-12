cc0a6ae17ae8d8aecdfb4962459fda706612210a
abhishekhirapara01 <hiraparaabhi07@gmail.com>
2024-03-12 17:30:19 +0530

    Changes in Project

--- project/app/models.py
+++ project/app/models.py
@@ -4,5 +4,6 @@ from django.db import models
 class Students(models.Model):
   Name =  models.CharField(max_length=100)
   Subjects = models.CharField(max_length=100)
+  Image = models.ImageField(upload_to= 'media/images/')
   def  __str__(self): 
     return self.Name
